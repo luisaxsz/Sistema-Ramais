b92991d01f5fa401f1a59bc156b5d81e39677d9c
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2024-03-09 15:16:35 -0300

    Sistema ok

--- js/firebase.js
+++ js/firebase.js
@@ -2,21 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebas
 import {getDatabase,ref,get, child} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHvlquMKHrNKWyb3BEoP4hvS_cUU_s6k8",
-
-  authDomain: "ramais-casa-civil.firebaseapp.com",
-
-  databaseURL: "https://ramais-casa-civil-default-rtdb.firebaseio.com",
-
-  projectId: "ramais-casa-civil",
-
-  storageBucket: "ramais-casa-civil.appspot.com",
-
-  messagingSenderId: "438982792661",
-
-  appId: "1:438982792661:web:d89e7f1733de49bbd6d95c",
-
-  measurementId: "G-1S7YGSS23D",
+ 
 };
 
 const app = initializeApp(firebaseConfig);
